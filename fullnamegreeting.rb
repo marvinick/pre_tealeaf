efbcd9b54c1340192e3e48ee0efdea237304f416
marvinick <marviluw@gmail.com>
2013-07-17 15:34:46 -0500

    ch5 full name greeting

---
+++ fullnamegreeting.rb
@@ -0,0 +1,7 @@
+puts 'OK first question, what is your first name?'
+name1 = gets.chomp 
+puts 'Next, what is your middle name?'
+name2 = gets.chomp
+puts 'Finally, last name?'
+name3 = gets.chomp 
+puts 'Welcome ' + name1 + name2 + name3 + '.' 
