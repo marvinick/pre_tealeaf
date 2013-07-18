33833a7767dd9fe4f81ea47993819211eefbb66d
marvinick <marviluw@gmail.com>
2013-07-18 16:01:26 -0500

    the right one

--- deafgrandma.rb
+++ deafgrandma.rb
@@ -1,9 +1,16 @@
-while true 
-	input = gets.chomp 
-	puts "Huh? Speak Sonny!"
-	if input = gets.chomp.capitalize
-		break 
-	end 
-end 
+while true
 
-puts "No, not since 1938" 
+said = gets.chomp
+	if said == "BYE"
+		puts 'BYE SWEETIE!' 
+	break
+	end
+	if said != said.upcase
+		puts 'HUH?! SPEAK UP, SONNY!'
+	else
+      random_year = 1930 + rand(21)
+	  puts 'NO, NOT SINCE ' + random_year.to_s + '!' 
+	end
+	end
+
+
